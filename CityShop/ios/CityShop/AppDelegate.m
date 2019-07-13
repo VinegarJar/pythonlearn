ded83c80aff60152402c8a9baa82d5cc01f8db39
ryp-app01 <ryp-app01@ryp-app01deiMac.local>
2019-07-13 11:36:10 +0800

    tests

--- CityShop/ios/CityShop/AppDelegate.m
+++ CityShop/ios/CityShop/AppDelegate.m
@@ -3,6 +3,8 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
+  RN 项目需要 Dead Code Stripping  设置为yes
+ 
  */
 
 #import "AppDelegate.h"
---
+++ CityShop/ios/bank/BankCard.h
@@ -0,0 +1,13 @@
+//
+//  BankCard.h
+//  CardScan
+//
+//  Created by Keith on 16/8/26.
+//  Copyright © 2016年 Keith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BankCard : UIViewController
+
+@end
