85e69592fed58a7f40d1292c568af30dc0c5110d
wxhwzi <2966497308@qq.com>
2019-07-14 11:18:37 +0800

    test

--- CityShop/ios/CityShop/AppDelegate.m
+++ CityShop/ios/CityShop/AppDelegate.m
@@ -3,17 +3,14 @@
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
-  RN 项目需要 Dead Code Stripping  设置为yes
-   pod  'Bugly'
+  RN 项目需要 Dead Code Stripping(第三方银行卡sdk)  设置为yes
+  #https://www.jianshu.com/p/c9c525e0c25f 移除项目中的cocoapods   
  */
 
 #import "AppDelegate.h"
-
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
 #import <React/RCTDevLoadingView.h>
-
-
 
 
 @implementation AppDelegate
